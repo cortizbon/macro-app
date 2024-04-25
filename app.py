ee189d3c1622300547bc9a00375d826839aa7ef4
Carlos Ortiz <carlosortiz@192.168.0.4>
2024-04-25 14:05:29 -0500

    first push

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import econpizza as ep
+
+from utils import read_model, test_shock, plot_results
+
+
+st.streamlit("Modelos de macro avanzada")
+
+tab1, tab2, tab3, tab4 = st.tabs(["Modelo básico",
+                                  "RBC",
+                                  "MIU",
+                                  "NeoKeynesian"])
+
+with tab1:
+    st.header("Modelo básico")
+    model = read_model("/yamls/model.yaml")
+    # ecuaciones de equilibrio
+
+    var_shock = st.selectbox("Seleccione el shock a evaluar: ", model.shocks)
+
+    _ = model.solve_stst()
+    # schock
+
+    x = test_shock(var_shock, model)
+
+    # ingreso de parámetros para análisis de estabilidad
+    # obtener resultados
+    plot_results(50, model, x)
+    pass
+
+with tab2:
+    st.header("Modelo RBC")
+    pass
+with tab3:
+    st.header("MIU")
+    pass
+with tab4:
+    st.header("NeoKeynesian")
+    pass
---
+++ utils.py
@@ -0,0 +1,26 @@
+import econpizza as ep
+import matplotlib.pyplot as plt
+import numpy as np
+
+def test_shock(var_shock, model):
+
+    shk = (var_shock, 0.01 ** 2)
+    x, flag = model.find_path(shock=shk)
+    return x
+
+def plot_results(num_simuls, model, x):
+
+    num_rows = np.ceil(len(model['variables']) / 3)
+
+    fig, axs = plt.suplots(num_rows, 3, figsize=(10, 4))
+    for i, v in enumerate(()):
+        axs.flatten()[i].plot(x[:num_simuls, model['variables']])
+        axs.flatten()[i].set_xlabel(v)
+    
+    plt.show()
+
+def read_model(yaml_name: str):
+    model = ep.load(yaml_name,
+                    raise_errors=True,
+                    verbose=True)
+    return model
